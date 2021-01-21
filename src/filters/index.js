fcdd8b679560c620e60aa171992cd2b4a85e7da4
Lshan523 <Lshan523@163.com>
2021-01-21 17:56:17 +0800

    add  date-fns

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,11 +1,15 @@
 import Vue from  'vue'
-import moment from 'moment'
+// import moment from 'moment'
+import format from 'date-fns/format'
 /**
  * 自定义过滤器
  * 格式化时间  cnpm isntall --save moment
  * 在main.js 中引用，加载 import './fiters'
  */
 
-Vue.filter('date-format',function (value,formatStr="yyyy-MM-DD HH:mm:ss") {
-  return moment(value).format(formatStr)
+Vue.filter('date-format',function (value,formatStr) {
+  //方式1 moment (依赖包比较大)
+  // return moment(value).format(formatStr || "yyyy-MM-DD HH:mm:ss")
+  //方式2 date-fns(依赖包相对小一些)
+  return format(value, formatStr || 'YYYY-MM-DD HH:mm:ss')
 })
